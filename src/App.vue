7e5c899251f14c525d6010aa74d630f317e7cb0c
Saiph <zhangwh754@gmail.com>
2021-10-08 15:59:59 +0800

    🔧 build: 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+  @import url('assets/css/base.css');
+</style>
