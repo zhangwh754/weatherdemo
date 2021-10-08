071331c8e9d34b7ff260d2294f16bb31a3e6be4e
Saiph <zhangwh754@gmail.com>
2021-10-08 16:31:56 +0800

    ✨ feat(Home): 完成Home的样式，使用写死的数据

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="app">
-
+    <home />
   </div>
 </template>
 
 <script>
+import Home from 'views/home/Home.vue'
 
 export default {
   name: 'App',
   components: {
-
+    Home
   }
 }
 </script>
